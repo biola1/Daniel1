1f5e64f3b38144edfca4ce9c8fee998604e60539
biola1 <abiolamajekodunmi2011@yahoo.com>
2023-03-19 00:00:12 -0500

    Correct Code

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -7,7 +7,7 @@ import org.mockito.internal.matchers.LessThan;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
@@ -45,6 +45,6 @@ public class TestGreeter {
   public void greetShouldIncludeGreetinginformation() {
     String someone = "Jesus, Thanks you making me Happy";
 
-    assertThat(greeter.greet(someone).length(), is(lessThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
